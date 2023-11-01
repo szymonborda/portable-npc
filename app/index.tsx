9479224f7c0509d118925449d15b8f710183e366
Szymon Borda <szymon.borda@deployed.pl>
2023-11-01 12:31:34 +0100

    add expo-router navigation

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar, View, Text } from "react-native";
+
+export default function App() {
+    return (
+        <View>
+            <Text>Hello, world!</Text>
+            <StatusBar />
+        </View>
+    );
+}
